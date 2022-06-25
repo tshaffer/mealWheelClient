600b1df52561f4746fc93058f00e4eadbf4e82c3
Ted Shaffer <shaffer.family@gmail.com>
2022-06-25 10:22:18 -0700

    added uploadFile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,15 +2,43 @@
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Box } from '@mui/material';
+import { uploadFile } from '../controllers/appState';
 
 export interface AppProps {
+  onUploadFile: (formData: FormData) => any;
 }
 
 const App = (props: AppProps) => {
 
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const handleFileChangeHandler = (e: any) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUploadFile = () => {
+    console.log('uploadFile: ', selectedFile);
+    const data = new FormData();
+    data.append('file', selectedFile);
+    props.onUploadFile(data);
+  };
+
   return (
     <div>
-      Pizza
+      <Box
+        component='form'
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete='off'
+      >
+        <input type="file" name="file" onChange={handleFileChangeHandler} />
+        <br />
+        <button type="button" onClick={handleUploadFile}>Upload</button>
+        <br />
+      </Box>
     </div>
   );
 };
@@ -22,6 +50,7 @@ function mapStateToProps(state: any) {
 
 const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({
+    onUploadFile: uploadFile,
   }, dispatch);
 };
 
