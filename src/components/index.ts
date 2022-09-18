31b5a32f1620f0b9f3132a6ff34ef56ce722dbb1
Ted Shaffer <shaffer.family@gmail.com>
2022-09-18 05:58:13 -0700

    Remove unused and example files

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export * from './Dishes';
 export * from './MealInCalendar';
 export * from './Home';
 export * from './Login';
-export * from './MealComponent';
 export * from './MealSchedule';
 export * from './ToolsAndSettings';
 
