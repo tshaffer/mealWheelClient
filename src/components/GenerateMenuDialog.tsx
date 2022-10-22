13f56b5f68d0a72ed9e63b7408bee83b642e435e
Ted Shaffer <shaffer.family@gmail.com>
2022-10-22 06:20:12 -0700

    ongoing progress...

--- src/components/GenerateMenuDialog.tsx
+++ src/components/GenerateMenuDialog.tsx
@@ -6,12 +6,17 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 
 import dayjs, { Dayjs } from 'dayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { isNil } from 'lodash';
 
 export interface GenerateMenuDialogPropsFromParent {
   open: boolean;
@@ -29,12 +34,30 @@ function GenerateMenuDialog(props: GenerateMenuDialogProps) {
   const [startDate, setStartDate] = React.useState<Dayjs | null>(
     dayjs('2022-10-22T21:11:54'),
   );
+  const [numberOfMealsToGenerate, setNumberOfMealsToGenerate] = React.useState<number>(7);
+  const [overwriteExistingMeals, setOverwriteExistingMeals] = React.useState<boolean>(true);
 
   const handleUpdateStartDate = (newValue: Dayjs | null) => {
     setStartDate(newValue);
   };
 
+  const handleUpdateNumberOfMealsToGenerate = (event: object) => {
+    const textEntry = ((event as any).target as any).value;
+    const numberOfMealsToGenerateEntry = parseInt(textEntry, 10);
+    setNumberOfMealsToGenerate(numberOfMealsToGenerateEntry);
+  };
+
+  const handleUpdateOverwriteExistingMeals = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOverwriteExistingMeals(event.target.checked);
+  };
+
   const handleClose = () => {
+
+    if (!isNil(startDate)) {
+      const startDateToReturn: Date = startDate?.toDate();
+      console.log('Start date: ', startDateToReturn);
+    }
+
     onClose();
   };
 
@@ -42,7 +65,9 @@ function GenerateMenuDialog(props: GenerateMenuDialogProps) {
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Generate Menu </DialogTitle>
       <DialogContent>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LocalizationProvider
+          dateAdapter={AdapterDayjs}
+        >
           <DesktopDatePicker
             label="Start Date"
             inputFormat="MM/DD/YYYY"
@@ -51,6 +76,31 @@ function GenerateMenuDialog(props: GenerateMenuDialogProps) {
             renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
+        <br />
+        <TextField
+          sx={{ m: 1, maxHeight: '40px', marginTop: '12px' }}
+          type='number'
+          label="Number of meals to generate"
+          InputProps={{
+            inputProps: { min: 1 }
+          }}
+          value={numberOfMealsToGenerate}
+          onChange={handleUpdateNumberOfMealsToGenerate}
+        />
+        <br />
+        <FormGroup
+          sx={{ m: 1, maxHeight: '40px', marginTop: '12px' }}
+        >
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={overwriteExistingMeals}
+                onChange={handleUpdateOverwriteExistingMeals}
+              />
+            }
+            label="Overwrite existing meals"
+          />
+        </FormGroup>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
@@ -69,3 +119,4 @@ function mapStateToProps(state: any) {
 }
 
 export default connect(mapStateToProps)(GenerateMenuDialog);
+
