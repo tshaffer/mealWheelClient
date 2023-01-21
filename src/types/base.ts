367cda299d838477b973c0fd717df6a76531f36d
Ted Shaffer <shaffer.family@gmail.com>
2023-01-21 05:47:28 -0800

    point back to localHost

--- src/types/base.ts
+++ src/types/base.ts
@@ -12,8 +12,8 @@ import {
 }
   from './entities';
 
-// export const serverUrl = 'http://localhost:8000';
-export const serverUrl = 'https://tsmealwheel.herokuapp.com';
+export const serverUrl = 'http://localhost:8000';
+// export const serverUrl = 'https://tsmealwheel.herokuapp.com';
 
 export const apiUrlFragment = '/api/v1/';
 
