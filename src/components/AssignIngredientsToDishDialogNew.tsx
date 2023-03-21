b4841ee13966add20a1335421fbb9c12ec732b0a
Ted Shaffer <shaffer.family@gmail.com>
2023-03-21 11:32:32 -0700

    checkpoint 1

--- src/components/AssignIngredientsToDishDialogNew.tsx
+++ src/components/AssignIngredientsToDishDialogNew.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+
+
 import { DishEntity } from '../types';
 import { addIngredientToDish, replaceIngredientInDish, deleteIngredientFromDish } from '../controllers';
 import { MealWheelDispatch } from '../models';
 import { getDishById, getIngredients, getIngredientsByDish } from '../selectors';
+import { isNil } from 'lodash';
 
 export interface AssignIngredientsToDishDialogNewPropsFromParent {
   open: boolean;
@@ -17,16 +27,47 @@ export interface AssignIngredientsToDishNewDialogProps extends AssignIngredients
 }
 
 function AssignIngredientsToDishNewDialog(props: AssignIngredientsToDishNewDialogProps) {
+
+  if (isNil(props.dishId) || props.dishId === '') {
+    return null;
+  }
+
+  const handleClose = () => {
+    props.onClose();
+  };
+
+  const dishLabel: string = isNil(props.dish) ? 'Unknown dish' : props.dish.name;
+
   return (
-    <div>poo</div>
+    <Dialog onClose={handleClose} open={props.open}>
+      <DialogTitle>Assign Ingredients to {dishLabel}</DialogTitle>
+      <DialogContent>
+        <Box
+          sx={{
+            height: 500,
+            width: '100%',
+            '& .actions': {
+              color: 'text.secondary',
+            },
+            '& .textPrimary': {
+              color: 'text.primary',
+            },
+          }}
+        >
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
 function mapStateToProps(state: any, ownProps: AssignIngredientsToDishDialogNewPropsFromParent) {
   return {
     dish: getDishById(state, ownProps.dishId),
-    allIngredients: getIngredients(state),
-    ingredientsInDish: getIngredientsByDish(state, ownProps.dishId),
+    // allIngredients: getIngredients(state),
+    // ingredientsInDish: getIngredientsByDish(state, ownProps.dishId),
   };
 }
 
@@ -39,5 +80,5 @@ const mapDispatchToProps = (dispatch: MealWheelDispatch) => {
 };
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(AssignIngredientsToDishDialogNew);
+export default connect(mapStateToProps, mapDispatchToProps)(AssignIngredientsToDishNewDialog);
 
