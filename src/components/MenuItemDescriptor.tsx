3f906c55b7c79a6f32ed0392b4d0aee18f80ec3b
Ted Shaffer <shaffer.family@gmail.com>
2023-05-29 05:06:38 -0700

    Fix accompaniment label display in MenuItemDescriptor.

--- src/components/MenuItemDescriptor.tsx
+++ src/components/MenuItemDescriptor.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { isNil } from 'lodash';
 
-import { DishEntity, MealEntity } from '../types';
+import { AccompanimentTypeNameById, DishEntity, MealEntity } from '../types';
+import { MealWheelDispatch } from '../models';
+import { getAccompanimentTypeNamesById } from '../selectors';
 
-export interface MenuItemProps {
+export interface MenuItemPropsFromParent {
   meal: MealEntity;
+}
+
+export interface MenuItemProps extends MenuItemPropsFromParent {
+  accompanimentTypeNameById: AccompanimentTypeNameById;
 }
 
 const MenuItemDescriptor = (props: MenuItemProps) => {
 
   const { meal } = props;
+
+  const getAccompanimentLabel = (accompanimentTypeEntityId: string): string => {
+    return props.accompanimentTypeNameById[accompanimentTypeEntityId];
+  };
 
   const renderDish = (dishEntity: DishEntity, dishLabel: string, insertBreak: boolean): JSX.Element => {
     return (
@@ -24,7 +36,8 @@ const MenuItemDescriptor = (props: MenuItemProps) => {
   const renderAccompaniments = (): JSX.Element[] => {
     if (!isNil(meal.accompanimentDishes)) {
       const accompaniments = meal.accompanimentDishes.map( (accompanimentDish: DishEntity) => {
-        return renderDish(accompanimentDish, accompanimentDish.type, true);
+        const accompanimentLabel = getAccompanimentLabel(accompanimentDish.type);
+        return renderDish(accompanimentDish, accompanimentLabel, true);
       });
       return accompaniments;
     }
@@ -41,4 +54,16 @@ const MenuItemDescriptor = (props: MenuItemProps) => {
   );
 };
 
-export default MenuItemDescriptor;
+function mapStateToProps(state: any, ownProps: MenuItemPropsFromParent) {
+  return {
+    accompanimentTypeNameById: getAccompanimentTypeNamesById(state),
+  };
+}
+
+const mapDispatchToProps = (dispatch: MealWheelDispatch) => {
+  return bindActionCreators({
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MenuItemDescriptor);
+
